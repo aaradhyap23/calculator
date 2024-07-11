651835565239c23832695d1c2682c802d7b42f11
aaradhyap23 <aaradhya23@iiserb.ac.in>
2024-07-11 12:41:39 +0530

    js almost done

--- app.js
+++ app.js
@@ -1,19 +1,46 @@
-function perform_operation(exp) {
-    nums = exp.match(/[0-9]+/g);
-    switch(exp.match(/[+*-/]/g)[0]){
-        case '+':
-            return Number(nums[0]) + Number(nums[1]);
-        case '-':
-            return Number(nums[0]) - Number(nums[1]);
-        case '*':
-            return Number(nums[0]) * Number(nums[1]);        
-        case '/':
-            return Number(nums[0]) / Number(nums[1]);
+function bodmas(exp) {
+  nums = exp.match(/[0-9]+/g);
+  op = exp.match(/[+*-/]/g);
+  if(op==null){
+    return exp
+  }
+  if(op!=null && nums.length==1){
+    return exp
+  }
+  while (op.indexOf("/") != -1) {
+    res = Number(nums[op.indexOf("/")]) / Number(nums[op.indexOf("/") + 1]);
+    nums[op.indexOf("/")] = res;
+    nums.splice(op.indexOf("/") + 1, 1);
+    op.splice(op.indexOf("/"), 1);
+  }
+  while (op.indexOf("*") != -1) {
+    res = Number(nums[op.indexOf("*")]) * Number(nums[op.indexOf("*") + 1]);
+    nums[op.indexOf("*")] = res;
+    nums.splice(op.indexOf("*") + 1, 1);
+    op.splice(op.indexOf("*"), 1);
+  }
+  while (op.indexOf("+") != -1) {
+    res = Number(nums[op.indexOf("+")]) + Number(nums[op.indexOf("+") + 1]);
+    nums[op.indexOf("+")] = res;
+    nums.splice(op.indexOf("+") + 1, 1);
+    op.splice(op.indexOf("+"), 1);
+  }
+  while (op.indexOf("-") != -1) {
+    res = Number(nums[op.indexOf("-")]) - Number(nums[op.indexOf("-") + 1]);
+    nums[op.indexOf("-")] = res;
+    nums.splice(op.indexOf("-") + 1, 1);
+    op.splice(op.indexOf("-"), 1);
+  }
+  return nums[0];
+}
+
+function solve(exp) {
+    const re = /\([^\(\)]+\)/
+    while(exp.match(re)!=null){
+        sol = bodmas(exp.match(re)[0].substring(1,exp.match(re)[0].length-1))
+        exp=exp.replace(exp.match(re)[0],sol)
+        console.log(exp)
     }
+    return bodmas(exp)
 }
-function bodmas(exp){
-    nums = exp.match(/[0-9]+/g)
-    op = exp.match(/[+*-/]/g)
-    
-}
-bodmas("12*65+4*6");
+console.log(solve("-1"));
